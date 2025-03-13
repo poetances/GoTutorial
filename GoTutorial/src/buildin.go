c78e77e1904577ead409d06712d7188cb18c7c31
zhucj3 <zhucj3@lenovo.com>
2025-03-13 20:55:19 +0800

    feat: 3.13

--- GoTutorial/src/buildin.go
+++ GoTutorial/src/buildin.go
@@ -13,7 +13,7 @@ func imag(c ComplexType) FloatType
 func len(v Type) int
 func make(Type, size IntegerType) Type
 func new(Type) *Type
-func panic(v interface{})
+func panic(v interface{}) 
 func real(c ComplexType) FloatType
 func recover() interface{}
 
